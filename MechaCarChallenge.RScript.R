2a8daa67b77acbcf1e1cce734066574f748c276a
Wengi <74740339+wengio3@users.noreply.github.com>
2021-03-21 22:03:06 -0400

    Update MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -7,7 +7,14 @@
 library(dplyr)
 
 # 4. Import and read in the MechaCar_mpg.csv file as a dataframe.
-demo_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+Mecha_car <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) 
+# 5. Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), and add the dataframe you created in Step 4 as the data parameter.
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_car)
+
+# 6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_car))
+
+# 7. Save your MechaCarChallenge.RScript file to your GitHub repository.
 
 
 ## Deliverable 2
@@ -15,6 +22,25 @@ demo_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = 
 # 1. Download the Suspension_Coil.csv file, and place it in the active directory for your R session.
 
 # 2. In your MechaCarChallenge.RScript, import and read in the Suspension_Coil.csv file as a table.
-## demo_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+cars_summary <- read.csv('Suspension_Coil.csv', stringsAsFactors = F)
 
 # 3. Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- cars_summary  %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
+lot_summary <- cars_summary %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
+                                                                       
