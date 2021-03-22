de4783eb535b9893355edacf23fdc766333b94cc
Wengi <74740339+wengio3@users.noreply.github.com>
2021-03-21 22:16:57 -0400

    Update MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -27,10 +27,27 @@ cars_summary <- read.csv('Suspension_Coil.csv', stringsAsFactors = F)
 # 3. Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
 total_summary <- cars_summary  %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
-
+# 4. Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
 lot_summary <- cars_summary %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
-                                                                       
-                                                                       
+
+# 5. Save your MechaCarChallenge.RScript file to your GitHub repository.
+
+## Deliverable 3
+                                
+# 1. In your MechaCarChallenge.RScript, write an RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(cars_summary$PSI,mu=1500)
+
+# 2. Next, write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+lot1 <- subset(cars_summary, Manufacturing_Lot=="Lot1")
+lot2 <- subset(cars_summary, Manufacturing_Lot=="Lot2")
+lot3 <- subset(cars_summary, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+
+# 3. Save your MechaCarChallenge.RScript file to your GitHub repository.
+
                                                                        
                                                                        
                                                                        
